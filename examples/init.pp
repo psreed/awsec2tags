48f14a4bceada4f5775bf52bd4cb028684a31010
Paul Reed <paul.reed@puppetlabs.com>
2017-08-15 10:37:11 -0400

    Update init.pp
    Add example for user-data.epp usage

--- examples/init.pp
+++ examples/init.pp
@@ -10,3 +10,26 @@
 # https://docs.puppet.com/guides/tests_smoke.html
 #
 include ::awsec2tags
+
+#ec2_instance { "myinstnace.example.com":
+#  ensure               => present,
+#  region               => "us-west-2",
+#  availability_zone    => "us-west-2a",
+#  image_id             => "ami-id",
+#  instance_type        => "t2.small",
+#  key_name             => "your-key-name",
+#  security_groups      => ['standard-access'],
+#  tags                 =>  {
+#    "created_by"       => "paul.reed",
+#    "department"       => "tse",
+#    "project"          => "AWS Customer Demos & Testing",
+#  },
+#  user_data            => epp('awsec2tags/user-data.epp', {
+#    'puppet_master'    => 'master.example.com',
+#    'agent_certname'   => 'mynewclient.example.com',
+#    'os_type'          => 'Linux or Windows',
+#    'pp_preshared_key' => 'key, if using autosign',
+#    'pp_cloudplatform' => 'aws',
+#    'pp_role'          => 'role::my-role-for-this-host',
+#  }),
+#}
