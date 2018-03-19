ad839bbb003147155ae349e0969e9a95911d736e
Paul Reed <paul@paulreed.ca>
2018-03-19 14:07:38 -0400

    updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,44 +6,15 @@
 # Currently supports Windows and Linux
 # Tested on Windows 2012 R2, RHEL7 and CentOS7
 #
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'awsec2tags':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
 # Authors
 # -------
 #
-# Author Name <author@domain.com>
+# Paul Reed <paul.reed@puppet.com>
 #
 # Copyright
 # ---------
 #
-# Copyright 2017 Your name here, unless otherwise noted.
+# Copyright 2018 Paul Reed, unless otherwise noted.
 #
 class awsec2tags (
   $aws_access_key_id = $awsec2tags::params::aws_access_key_id,
@@ -53,7 +24,7 @@ class awsec2tags (
   $clean_up_aws_gems = $awsec2tags::params::clean_up_aws_gems,
 ) inherits awsec2tags::params {
 
-  #Currently only works on Linux
+  #Cleanup gems for aws-sdk* currently only works on Linux
   if $facts['os']['family'] != 'windows' {
     if $clean_up_aws_gems == true {
       exec { "${gem_bin} list | ${grep_cmd} \"aws-sdk\" | cut -d\" \" -f1 | xargs ${gem_bin} uninstall -aIx":
@@ -77,9 +48,6 @@ class awsec2tags (
   }
   -> file { $awsec2tags::ini_file:
     ensure  => file,
-    content => "[default]
-aws_access_key_id = ${aws_access_key_id}
-aws_secret_access_key = ${aws_secret_access_key}
-"
+    content => "[default]\naws_access_key_id = ${aws_access_key_id}\naws_secret_access_key = ${aws_secret_access_key}\n"
   }
 }
